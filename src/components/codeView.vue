642e4072fab951c43b4b0de1bceb5189f209c88b
henuzzw <1174371035@qq.com>
2023-06-14 13:50:36 +0800

    错误定位按钮和提交代码按钮

--- src/components/codeView.vue
+++ src/components/codeView.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="codeView">
-    <codemirror-demo></codemirror-demo>
+<!--    <codemirror-demo></codemirror-demo>-->
+
+    <div class="confirmButton">
+      <el-button type="primary">错误定位</el-button>
+      <el-button type="Info">提交代码</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 import { Codemirror } from 'vue-codemirror';
+import {ElTree,ElCheckboxGroup,ElCheckbox ,ElButton} from 'element-plus'
+import {ref, reactive} from "vue";
 
 
 // import base style
@@ -16,12 +23,11 @@ import 'codemirror/mode/javascript/javascript.js'
 import 'codemirror/theme/base16-dark.css'
 // import more 'codemirror/some-resource...'
 import codemirrorDemo from "./codemirrorDemo";
-import {ref} from 'vue'
 
 export default {
   name: "codeView",
   components: {
-    codemirrorDemo
+    codemirrorDemo, ElCheckboxGroup,ElCheckbox,ElButton,ElTree
   },
   setup() {
 
@@ -33,7 +39,13 @@ export default {
 .codeView {
   margin: 0 10px;
   width: 60%;
+  position: relative;
 
   background-color: aliceblue;
+  .confirmButton{
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+  }
 }
 </style>
